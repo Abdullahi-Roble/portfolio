f19f5369e011dbc4ee75ee866cb781c3596f2c24
Abdullah <abdullahi.roble7@gmail.com>
2019-09-22 22:47:22 -0400

    fif

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+const titles = {
+    strings: [" Front-End Software Engineer.", " Technology Enthusiast.", "Indoor Sportsman.", " Front-End Developer."],
+    startDelay: 1000,
+    typeSpeed: 50,
+    backSpeed: 40,
+}
+const typed = new Typed('.titleType', titles);
+
+$(document).ready(function(){
+
+
+    AOS.init();
+});
