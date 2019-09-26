fb5cdfb89f5796ef7558b30de3f335a5227ad614
Abdullah <abdullahi.roble7@gmail.com>
2019-09-25 23:54:27 -0400

    AOS added

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,47 @@
 
 // Implementing typed.js into header
 
-const letters = {
-    strings: ["Nice to meet you! I'm Abdullah.", "Nice to meet you! I'm Abdullahi."],
-    startDelay: onscroll,
-    typeSpeed: 50,
-    backSpeed: 40,
-    loop: false,
-    backDelay: 1000
-};
-
-const one = new Typed('.loneLetter', letters);
-
 const titles = {
     strings: ["Front-End Developer", "Technology Enthusiast", "Sportsman/Sports Fan", "Front-End Developer"],
     startDelay: 500,
     typeSpeed: 50,
     backSpeed: 40,
     loop: false,
-    backDelay: 1000
+    backDelay: 1000,
+    showCursor: true
 };
 
 const typed = new Typed('.titleType', titles);
 
+// Typed.js implementation into 'About' section
+
+const letters = {
+    strings: ["Nice to meet you! I'm Abdullah.", "Nice to meet you! I'm Abdullahi."],
+    startDelay: 5000,
+    typeSpeed: 50,
+    backSpeed: 40,
+    loop: false,
+    // loopCount: 3,
+    backDelay: 1000,
+    showCursor: true,
+};
+
+const one = new Typed('.loneLetter', letters);
+
 
 // Initiating flickity for portfolio works
 
-const elem = document.querySelector('.galleryWorks');
-const flkty = new Flickity(elem, {
+const carousel = document.querySelector('.galleryWorks');
+const flkty = new Flickity(carousel, {
     // options
     cellAlign: 'left',
     contain: true,
-    wrapAround: true
+    wrapAround: true,
+    // freeScroll: true,
+    imagesLoaded: true,
+    percentPosition: false
 });
 
-$(document).ready(function(){
-    AOS.init();
-});
+// Animate on Scroll!
+
+AOS.init();
